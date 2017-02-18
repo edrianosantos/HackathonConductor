b36f8161799affa07f8cf495ff1bf96b8fc4c7a3
Felipe Silva <felipe.silwa@gmail.com>
2017-02-18 15:02:20 -0300

    Configuração para mapeamento

---
+++ PaymentRating/PaymentRating/PaymentRating/Context/BancoContext.cs
@@ -0,0 +1,22 @@
+﻿using PaymentRating.Mapping;
+using PaymentRating.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PaymentRating.Context
+{
+    public class BancoContext : DbContext
+    {
+        
+
+        public DbSet<Avaliacao> Avaliacoes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AvaliacaoMapping());
+        }
+    }
+}
--- PaymentRating/PaymentRating/PaymentRating/Mapping/AvaliacaoMapping.cs
+++ PaymentRating/PaymentRating/PaymentRating/Mapping/AvaliacaoMapping.cs
@@ -4,16 +4,16 @@ using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using PaymentRating.Models;
+using System.Data.Entity.ModelConfiguration;
 
 namespace PaymentRating.Mapping
 {
-    public class AvaliacaoMapping : DbContext
+    public class AvaliacaoMapping : EntityTypeConfiguration<Avaliacao>
     {
-        public DbSet<Avaliacao> Avaliacoes { get; set; }
-
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        public AvaliacaoMapping()
         {
-            modelBuilder.Entity<Avaliacao>().ToTable("Avaliacoes");
+            this.ToTable("Avaliacoes");
+            this.Property(t => t.Id).HasColumnName("Id");
         }
     }
 }
